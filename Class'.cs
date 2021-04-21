62ea989a595379b493b1da118dc188f0645530e3
Esroge <esroqge_bjk@hotmail.com>
2021-04-22 01:01:04 +0300

    Verileri txt'ye yazdırma.

---
+++ Class'.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depremler
+{
+    class Class_
+    {
+        public void TextOlustur()
+        {
+            FileStream fs = File.Create("depremler.txt");
+            fs.Close();
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,19 +14,29 @@ namespace Depremler
         {
             int total=0;
 
+            Class_ class_ = new Class_();
+            class_.TextOlustur();
+            
             string Site = "http://www.earthquakenewstoday.com/feed/";
             XmlDocument Veri = new XmlDocument();
             Veri.Load(Site);
 
             XmlNodeList Liste = Veri.GetElementsByTagName("title");
             total = Liste.Count;
-            
+
+            List<string> satırlar = new List<string>();
+            satırlar = File.ReadAllLines(@"depremler.txt").ToList();
+            foreach (string satır in satırlar)
+            {
+                Console.WriteLine(satır);
+            }
+           
             for (int i = 0; i < Liste.Count; i++)
             {
-                Console.WriteLine(Liste[i].InnerText.ToString());
+                satırlar.Add(Liste[i].InnerText.ToString());
+                File.WriteAllLines(@"depremler.txt", satırlar);
             }
-            Console.WriteLine(Environment.NewLine);
-            Console.ReadKey();
+            Console.WriteLine("Veriler 'Depremler.txt'ye yazıldı.");
         }   
     }
 }
