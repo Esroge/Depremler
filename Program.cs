35e089e0527df0d2a2fe9c9a8b7f8bf788ed89b4
Esroge <esroqge_bjk@hotmail.com>
2021-04-21 22:24:46 +0300

    Veri çektim ve döngü açtım.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
 
 namespace Depremler
 {
@@ -10,8 +12,18 @@ namespace Depremler
     {
         static void Main(string[] args)
         {
+            int total=0;
 
+            string Site = "http://udim.koeri.boun.edu.tr/zeqmap/xmlt/son24saat.xml";
+            XmlDocument Veri = new XmlDocument();
+            Veri.Load(Site);
 
+            XmlNodeList Liste = Veri.GetElementsByTagName("Name");
+            total = Liste.Count;
+            for (int i = 0; i < Liste.Count; i++)
+            {
+
+            }
         }
     }
 }
