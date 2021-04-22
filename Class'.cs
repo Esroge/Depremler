5921c55fea8a413c37e880e00cd65e6727058d59
Esroge <esroqge_bjk@hotmail.com>
2021-04-23 00:16:39 +0300

    classları değiştirdim, aldığım veriyi çoğalttım, kopyalamayı ekledim ve karşılaştırmayı düzelttim buglar var sonraki güncellemede düzelticeğim.

--- Class'.cs
+++ Class'.cs
@@ -10,36 +10,32 @@ namespace Depremler
 {
     class Class_
     {
-        int total = 0;
-        public void TextOlustur(string yol)
+        
+        public void TextOlustur(string y)
         {
-            FileStream fs = File.Create(yol);
+            FileStream fs = File.Create(y);
             fs.Close();
         }
 
-        public void Vericekme(string yol)
+         public void veriOkuma(string yol)
         {
-            string Site = "http://www.earthquakenewstoday.com/feed/";
-            XmlDocument Veri = new XmlDocument();
-            Veri.Load(Site);
-            XmlNodeList Liste = Veri.GetElementsByTagName("title");
-            total = Liste.Count;
-
+            XmlTextReader metin = new XmlTextReader("http://www.earthquakenewstoday.com/feed/");
             List<string> satırlar = new List<string>();
             satırlar = File.ReadAllLines(yol).ToList();
-            foreach (string satır in satırlar)
+            while (metin.Read())
             {
-                Console.WriteLine(satır);
-            }
-
-            for (int i = 0; i < Liste.Count; i++)
-            {
-                satırlar.Add(Liste[i].InnerText.ToString());
-                File.WriteAllLines(yol, satırlar);
-            }
-            if (yol == @"depremler.txt")
-            {
-                Console.WriteLine("Veriler 'Depremler.txt'ye yazıldı.");
+                if (metin.NodeType == XmlNodeType.Element && metin.Name == "title")
+                {
+                    string s1 = metin.ReadElementString();
+                    satırlar.Add(s1.ToString());
+                    File.WriteAllLines(yol, satırlar);
+                }
+                if (metin.NodeType == XmlNodeType.Element && metin.Name == "pubDate")
+                {
+                    string s2 = metin.ReadElementString();
+                    satırlar.Add(s2.ToString());
+                    File.WriteAllLines(yol, satırlar);
+                }
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -13,46 +13,71 @@ namespace Depremler
         static void Main(string[] args)
         {
             string yol = @"depremler.txt";
-            if (File.Exists(@"depremler.txt"))
-            {
-                Console.WriteLine("Depremler.txt zaten var.");
-                goto Karsilastirma;
-            }
+            string yol1 = @"degisim.txt";
             Class_ textolustur = new Class_();
             textolustur.TextOlustur(yol);
+            Class_ okuma = new Class_();
+            okuma.veriOkuma(yol);
+            Class_ yazma = new Class_();
 
-            Class_ verioku = new Class_();
-            verioku.Vericekme(yol);
+            if (File.Exists(@"degisim.txt"))
+            {
+                goto Karşılaştırma;
+            }
 
-        Karsilastirma:
-            string yol1 = @"degisim.txt";
+        
             Class_ degtextolustur = new Class_();
             degtextolustur.TextOlustur(yol1);
+        Kopyalama:
+            StreamReader oku = new StreamReader(yol);
+            List<string> satırlar = new List<string>();
+            satırlar = File.ReadAllLines(yol).ToList();
 
-            Class_ verioku1 = new Class_();
-            verioku1.Vericekme(yol1);
-
-            FileStream fs1 = new FileStream(yol, FileMode.Open, FileAccess.Read);
-            StreamReader sw = new StreamReader(fs1);
-            string yazi = sw.ReadLine();
-            FileStream fs2 = new FileStream(yol1, FileMode.Open, FileAccess.Read);
-            StreamReader sw2 = new StreamReader(fs2);
-            string yazi2 = sw2.ReadLine();
-            while (true) 
+            for (int i = 0; i < satırlar.Count; i++)
             {
-                Console.Clear();
-                if (yazi == yazi2)
-                {
-                    Console.WriteLine("degisimyok");
-                    break;
-                }
-                else 
-                {
-                    Console.WriteLine("Yeni bir deprem olmuş.");
-                    break;
-                }
+                string metin = oku.ReadLine();
+                File.WriteAllLines(yol1, satırlar);
+            }
+
+        Karşılaştırma:
+            StreamReader oku1 = new StreamReader(@"depremler.txt");
+            StreamReader oku2 = new StreamReader(@"degisim.txt");
+            string veri1 = oku1.ReadToEnd();
+            string veri2 = oku2.ReadToEnd();
+            if (veri1 == veri2)
+            {
+                Console.WriteLine("veriler aynı.");
+            }
+            else
+            {
+                Console.WriteLine("Veriler değişmiş.");
+                Console.ReadKey();
+                goto Kopyalama;
             }
             Console.ReadKey();
+
+            //FileStream fs1 = new FileStream(yol, FileMode.Open, FileAccess.Read);
+            //StreamReader sw = new StreamReader(fs1);
+            //string yazi = sw.ReadLine();
+            //FileStream fs2 = new FileStream(yol1, FileMode.Open, FileAccess.Read);
+            //StreamReader sw2 = new StreamReader(fs2);
+            //string yazi2 = sw2.ReadLine();
+            //while (true) 
+            //{
+            //    Console.Clear();
+            //    if (yazi == yazi2)
+            //    {
+            //        Console.WriteLine("degisimyok");
+            //        break;
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Yeni bir deprem olmuş.");
+            //        break;
+            //    }
+            //}
+            //Console.ReadKey();
+
         }   
     }
 }
